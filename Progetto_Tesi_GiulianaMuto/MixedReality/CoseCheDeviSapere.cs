3b2a89d674f12cdadba4b324294d39ad0336cdd1
Giuliana <43480122+giulianamuto@users.noreply.github.com>
2020-03-18 11:50:28 +0100

    parte 12

---
+++ Progetto_Tesi_GiulianaMuto/MixedReality/CoseCheDeviSapere.cs
@@ -0,0 +1,54 @@
+﻿/*
+ Ciao :-) Sono Giuliana è ho iniziato questo progetto, ti consiglio di scrivere qui ogni cosa e
+ commentare il codice che modifichi o scrivi per fare in modo che chi lavorerà dopo di te
+ non debba decifrare quello che tu hai fatto!! So che è un pò scocciante però per la riucita
+ di questo progetto devi farlo ;-) Puoi anche commentare questo script così tutti sanno quello che è stato modoficato
+ dall'inizio.
+ 
+ 
+ Tante informazioni sui plug in usati ecc le trovi sulla mia tesi quindi evito un copia e incolla ma ti dico le
+ cose più importanti che devi assolutamente sapere.
+ 
+ Inizio col dirti che in questo plug in di Vive quasi nulla è commentato e per me è stato un duro lavoro
+ ma ti assicuro che con un pò di impegno c'è la farai anche tu.
+
+ Ti consiglio di leggere la mia tesi (può dartela la dottoressa Paola Barra o se mi contatti per email te la invio)
+ specialmente il capitolo 2 e 3 che parlano del lavoro che ho svolto nei minimi dettagli
+
+ Detto ciò, iniziamo!! 
+
+ *************************************************************************************************************
+ Leggi la documentazione eprchè ti spiega tante cose !! 
+ Appena aprirai il progetto ci saranno degli errori
+ ma la documetanzione dice che se si presentano quegli errori bisogna ignorarli
+  *************************************************************************************************************
+  Cose che non sono riuscita a modificare:
+ - Quando parte le scanner se uno dei joystick non è acceso non funziona
+ *************************************************************************************************************
+ Quando parte l'applicaizone puoi notare che c'è quando scannerizza la scena vedi lo spatial scan
+ PER TOGLIERNO  prefab ViveSR in RididReconstrucctor e dpve ci sono quei due materiali li togli e inseirisci 
+ un materiale che si chiama "New Matirial" (orignale vero?)
+ 
+ CONSIGLIO: 
+ ti consiglio di toglierlo solo nella fase finale per provarlo e poi rimetterli perchè
+ è utile per capire cosa stai scannerizzando e se sta scannerizando
+ *************************************************************************************************************
+ Gli script con i metodi modificati sono:
+
+ ViveSR_DualCameraRing ------> Metodo SetMode 
+ Sample9S_SemanticSegmentation ------> Quasi Tutto
+ ViveSR_SceneUnderstanding --------> GetElementsBoundingBoxMeshes,GetElementsIcons
+ Quest'ultimo è molto importante è ti consiglio di leggere sulla mia tesi la parte in cui parlo del riconoscimento degli oggetti
+ li spiego come funzionano le mesh cube in unity e quindi potrai capire GetElementsBoundingBoxMeshes al meglio
+
+ Per entrare nel dettaglio cerca questi scirpt perchè li ho commentati
+ 
+ *************************************************************************************************************
+ Gli script che ho scritto sono:
+ -InstanziatePrefabs
+ *************************************************************************************************************
+ Con questo ho finito, buon lavoro !!!
+ 
+
+ Giuliana
+ */
---
+++ Progetto_Tesi_GiulianaMuto/MixedReality/InstanziatePrefabs.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using static Vive.Plugin.SR.SceneUnderstandingObjects;
+
+public class InstanziatePrefabs : MonoBehaviour
+{
+
+
+    //i prefabs degli oggetti 3D
+    public GameObject pref_chair, pref_wall, pref_ceiling, pref_table, pref_floor;
+ 
+
+    public void InitializationPrefabs(String element, Vector3 position, Vector3 scale, GameObject obj)
+    {
+       // Debug.Log("Sto inizializando i prefab");
+       
+          //In base all'oggetto che trovo creo il prefab corrispondente
+          //mi definisco la posizione
+          //creo il prefab
+          //modifico la scala del prefab
+          //rendo il prefab figlio del meshcube
+          //Eccezzione di floor: gli ho dato un valore nell'asse delle Y perchè così si trova un poco più in alto del pavimento
+
+        if (element.Equals("Chair"))
+        {
+            position = new Vector3(position.x, position.y, position.z);
+            pref_chair = Instantiate(pref_chair, position, new Quaternion(0, 0, 0, 0));
+            pref_chair.transform.localScale = scale;
+            pref_chair.transform.SetParent(obj.transform);
+            
+        }
+
+        if (element.Equals("Wall"))
+        {
+            position = new Vector3(position.x, position.y, position.z);
+            pref_wall = Instantiate(pref_wall, position, new Quaternion(0, 0, -90, 0));
+            pref_wall.transform.localScale = new Vector3(-0.111f, scale.y, scale.z);
+            pref_wall.transform.SetParent(obj.transform);
+
+            
+        }
+
+        if (element.Equals("Ceiling"))
+        {
+            position = new Vector3(position.x,position.y , position.z);
+            pref_ceiling = Instantiate(pref_ceiling, position,new Quaternion(0,0,180,0));
+            pref_ceiling.transform.localScale = scale;
+            pref_ceiling.transform.SetParent(obj.transform);
+        }
+
+        if (element.Equals("Table"))
+        {
+
+            position = new Vector3(position.x, position.y, position.z);
+            pref_table = Instantiate(pref_table, position, Quaternion.identity);
+            pref_table.transform.localScale = scale;
+            pref_table.transform.SetParent(obj.transform);
+        }
+
+ 
+        if (element.Equals("Floor"))
+        {
+            position = new Vector3(position.x, 0.40f, position.z);
+            pref_floor = Instantiate(pref_floor, position, Quaternion.identity);
+            pref_floor.transform.localScale = scale;
+            pref_floor.transform.SetParent(obj.transform);
+
+        }
+    }
+}
